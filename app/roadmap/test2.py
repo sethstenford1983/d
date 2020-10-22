688a390abb12f3e2aaee8ac75f51e8feec4b0ec3
seth <sethkill@mail.ru>
2020-10-22 21:47:55 +0300

    15.10.2020

--- app/roadmap/test2.py
+++ app/roadmap/test2.py
@@ -3,3 +3,16 @@ def f(y):
     y[0] = 25
 f(x)
 print(x)
+
+def r():
+    ls = [4,[7,8],6]
+    a,(b,c),d = ls
+    print(b,d)
+r()
+def rt():
+    ls = [ (3, [9, 1]), [(23,43), [5, 4]] ]
+    v = 0
+    for a,(g,j)in ls:
+        v = v + g
+        print(v)
+rt()
