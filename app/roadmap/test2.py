6fcbca2d101ed30cc9cbf6e500ed77949e33c0ef
seth <sethkill@mail.ru>
2020-10-29 19:42:18 +0300

    15.10.2020

--- app/roadmap/test2.py
+++ app/roadmap/test2.py
@@ -1,14 +1,14 @@
 x = [1,2,3]
 def f(y):
     y[0] = 25
-
+dfhdjhfjdhfjdhfjhdjfhjdhf
 def r():
     ls = [4,[7,8],6]
     a,(b,c),d = ls
     print(b,d)
-r()
+r()kjkdnfeeujjhdhgfdhhfhdj
 def rt():
-    ls = [ (3, [9, 1]), [(23,43), [5, 4]] ]
+    ls = [ (3, [9, 1]), [(23,43), [5, 4]] ]h dfdjhfjdhfjds hdjsahlhfdahljfh d
     v = 0
-
+dhfjdhfjdhfjdhfjdhfjhdjfhjdf
 rt()
