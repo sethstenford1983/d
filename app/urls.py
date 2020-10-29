ff8e6bbe7ddb481d6208ba57bb83e776a31ac60f
seth <sethkill@mail.ru>
2020-10-29 21:59:16 +0300

    15.10.2020

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from app.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('main_page', index),
     path('ma',login_user),
@@ -8,7 +10,4 @@ urlpatterns = [
     path('main_page1', index1),
     path('main_page2',index2),
     path('ajax_path', ajax_path)
-
-
-
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
